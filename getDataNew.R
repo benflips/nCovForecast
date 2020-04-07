03c1ecf8fb6a47c0593d108eb962557aa97819f9
Melissa Makin <melissa.makin@unimelb.edu.au>
2020-04-07 10:05:43 +1000

    caching Global and Australia data separately

--- getDataNew.R
+++ getDataNew.R
@@ -1,4 +1,4 @@
-## ---------------------------
+s## ---------------------------
 ##
 ## Script name: getData.R
 ##
@@ -27,9 +27,8 @@ source('functions.R')
 
 ## ---------------------------
 
-
 ## Get data
-server <- TRUE ## if you are drawing data directly over internet, set this to FALSE to use url alternatives:
+server <- FALSE ## if you are drawing data directly over internet, set this to FALSE to use url alternatives:
 if (server){
   tsConf  <- "/srv/shiny-server/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv" 
   tsDeath <- "/srv/shiny-server/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv" 
@@ -38,10 +37,8 @@ if (server){
   tsDeath <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 }
 
-
 tsI<-read_csv(file = tsConf)
 tsD<-read_csv(file = tsDeath)
-#tsT<-read_csv(file = tsTesting)
 
 ## get Date range
 dCols<-dateCols(tsI)
@@ -50,26 +47,51 @@ dates<-as.Date(colnames(tsI)[dCols], format = "%m/%d/%y")
 ## Tidy up names
 names(tsI)[!dCols] <- make.names(names(tsI)[!dCols])
 names(tsD)[!dCols] <- make.names(names(tsD)[!dCols])
-#names(tsT)[!dCols] <- make.names(names(tsT)[!dCols])
 
 ## add recovery lag -- assumes all cases recover at 22 days
 tsA <- recLag(tsI, tsD)
 
 
-tsICountry <- countryAgg(tsI) # aggregated to country
-tsACountry <- countryAgg(tsA) 
-tsDCountry <- countryAgg(tsD)
+## GLOBAL
 
-## Define menus
-# get region names with 20 or more cases as of yesterday
-ddNames <- tsACountry$Country[tsACountry[[ncol(tsACountry)-1]]>19]
+  tsICountry <- countryAgg(tsI) # aggregated to country
+  tsACountry <- countryAgg(tsA) 
+  tsDCountry <- countryAgg(tsD)
 
-ddReg <- ddNames
-names(ddReg) <- ddNames
+  ## Define menus
+  # get region names with 20 or more cases as of yesterday
+  ddNames <- tsACountry$Country[tsACountry[[ncol(tsACountry)-1]]>19]
 
-## run deconvolution to estimate undiagnosed cases
-source("detection/estGlobalV2.R")
+  ddReg <- ddNames
+  names(ddReg) <- ddNames
 
-## write data caches out
-save(ddReg, ddNames, file = "dat/menuData.RData")
-save(tsI, tsD, tsA, tsACountry, dates, ddNames, ddReg, file = paste0("dat/cacheData.RData"))
+  ## run deconvolution to estimate undiagnosed cases
+  source("detection/estGlobalV2.R")
+
+  ## write data caches out
+  save(ddReg, ddNames,                                   file =        "dat/Global/menuData.RData")
+  save(tsI, tsD, tsA, tsACountry, dates, ddNames, ddReg, file = paste0("dat/Global/cacheData.RData"))
+
+## AUSTRALIA
+
+  focusCountry <- 'Australia'
+
+  # subset to Australia
+  tsI <- subset(tsI, tsI$Country.Region == focusCountry)
+  tsD <- subset(tsD, tsD$Country.Region == focusCountry)
+
+  tsA <- recLag(tsI, tsD)
+
+  tsA <- natAgg(tsA)
+  tsI <- natAgg(tsI)
+  tsD <- natAgg(tsD)
+
+  ## Define menus
+  # get region names 
+  ddNames      <- tsA$Province.State
+  ddReg        <- ddNames
+  names(ddReg) <- ddNames
+
+  ## write data caches out
+  save(ddReg, ddNames,                                   file =        "dat/Australia/menuData.RData")
+  save(tsI, tsD, tsA, tsACountry, dates, ddNames, ddReg, file = paste0("dat/Australia/cacheData.RData"))
