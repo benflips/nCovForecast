3c58c11a9ac8a559aabdc792221e9a6654b6dc67
Melissa Makin <melissa.makin@unimelb.edu.au>
2020-06-02 13:31:27 +1000

    added width and height to Australian flag

--- app.R
+++ app.R
@@ -59,6 +59,8 @@ server <- function(input, output, session) {
 
   output$flagAustralia <- renderImage({
     list(src = normalizePath(file.path('./img/australia-flag-xs.png')),
+         width=100,
+         height=50,
          alt = 'Australian flag')
   }, deleteFile = FALSE)
 
