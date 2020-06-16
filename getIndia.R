da804c6095dc655d7f2ecfe2da334960b88898de
Melissa Makin <melissa.makin@unimelb.edu.au>
2020-06-16 12:00:11 +1000

    moving to better system of Region column only

--- getIndia.R
+++ getIndia.R
@@ -4,4 +4,4 @@ getDataGeneral('India',
                'https://raw.githubusercontent.com/vipinbhatnagar/covid19/master/confirmed.csv',
                'https://raw.githubusercontent.com/vipinbhatnagar/covid19/master/deaths.csv',
                '',
-               FALSE,FALSE)
+               FALSE,FALSE,TRUE)
