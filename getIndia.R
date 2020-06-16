882f170be73de4cd7c98ef63d88f1ce98c396202
Melissa Makin <melissa.makin@unimelb.edu.au>
2020-06-16 16:14:21 +1000

    getIndia now needs two fewer arguments

--- getIndia.R
+++ getIndia.R
@@ -4,4 +4,4 @@ getDataGeneral('India',
                'https://raw.githubusercontent.com/vipinbhatnagar/covid19/master/confirmed.csv',
                'https://raw.githubusercontent.com/vipinbhatnagar/covid19/master/deaths.csv',
                '',
-               FALSE,TRUE,TRUE)
+               TRUE)
