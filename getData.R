181f6fbefec5b5215d940c7802bb9b813fe0907c
benflips <benflips@users.noreply.github.com>
2020-06-25 10:48:05 +0800

    Added switch to getDatGeneral.R to choose between old and new deconvolution scripts.  Applied new to Australia only for now

--- getData.R
+++ getData.R
@@ -27,4 +27,5 @@ getDataGeneral('Australia',
                'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
                'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv',
                'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv',
-               TRUE)
+               TRUE,
+               deconvProcess =2)
