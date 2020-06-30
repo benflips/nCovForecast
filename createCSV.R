d6e3ae05515be981a002adc3c097c74d59d0ee5b
Melissa Makin <melissa.makin@unimelb.edu.au>
2020-06-30 11:01:13 +1000

    now write both confirmed and deaths to csv files in csvData

--- createCSV.R
+++ createCSV.R
@@ -4,16 +4,23 @@ library(tidyverse)
 createCSV <- function(countryName) {
 
 originalData <- covid19(c(countryName), level = 2)
-dataConfirmed <- originalData %>% select(administrative_area_level_2, date, confirmed)
 
-states <- unique(dataConfirmed$administrative_area_level_2)
-dates <- unique(dataConfirmed$date)
+states <- unique(originalData$administrative_area_level_2)
+dates <- unique(originalData$date)
 seq <- c(1:length(dates))
 
+dataConfirmed <- originalData %>% select(administrative_area_level_2, date, confirmed)
 outputConfirmed <- data.frame('Province.State' = states)
 for (value in seq) {
   outputConfirmed[[format(dates[value], '%-m/%d/%y')]] <- subset(dataConfirmed, dataConfirmed$date == dates[value])$confirmed
 }
-write.csv(outputConfirmed, paste0(countryName, '_confirmed.csv'), quote=FALSE)
+write.csv(outputConfirmed, paste0('csvData/',countryName, '_confirmed.csv'), quote=FALSE)
+
+dataDeaths <- originalData %>% select(administrative_area_level_2, date, deaths)
+outputDeaths <- data.frame('Province.State' = states)
+for (value in seq) {
+  outputDeaths[[format(dates[value], '%-m/%d/%y')]] <- subset(dataConfirmed, dataConfirmed$date == dates[value])$confirmed
+}
+write.csv(outputDeaths, paste0('csvData/',countryName, '_deaths.csv'), quote=FALSE)
 
 }
