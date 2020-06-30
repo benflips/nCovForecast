65893874376b098066017da394a1e07b8f19b230
Melissa Makin <melissa.makin@unimelb.edu.au>
2020-06-30 12:18:45 +1000

    also recovered data, and Colombia

--- createCSV.R
+++ createCSV.R
@@ -9,6 +9,7 @@ states <- unique(originalData$administrative_area_level_2)
 dates <- unique(originalData$date)
 seq <- c(1:length(dates))
 
+#confirmed
 dataConfirmed <- originalData %>% select(administrative_area_level_2, date, confirmed)
 outputConfirmed <- data.frame('Province/State' = states, 'Country/Region' = replicate(length(states),countryName))
 for (value in seq) {
@@ -16,11 +17,23 @@ for (value in seq) {
 }
 write.csv(outputConfirmed, paste0('csvData/',countryName, '_confirmed.csv'), quote=FALSE, row.names=FALSE)
 
+#deaths
 dataDeaths <- originalData %>% select(administrative_area_level_2, date, deaths)
 outputDeaths <- data.frame('Province/State' = states, 'Country/Region' = replicate(length(states),countryName))
 for (value in seq) {
   outputDeaths[[format(dates[value], '%-m/%d/%y')]] <- subset(dataDeaths, dataDeaths$date == dates[value])$deaths
 }
 write.csv(outputDeaths, paste0('csvData/',countryName, '_deaths.csv'), quote=FALSE, row.names=FALSE)
+
+
+#recovered
+dataRecovered <- originalData %>% select(administrative_area_level_2, date, recovered)
+outputRecovered <- data.frame('Province/State' = states, 'Country/Region' = replicate(length(states),countryName))
+for (value in seq) {
+  outputRecovered[[format(dates[value], '%-m/%d/%y')]] <- subset(dataRecovered, dataRecovered$date == dates[value])$recovered
+}
+write.csv(outputRecovered, paste0('csvData/',countryName, '_recovered.csv'), quote=FALSE, row.names=FALSE)
+
+
 
 }
