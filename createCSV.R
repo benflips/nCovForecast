e948b63e9fe3849f3ada29ed11153db6586e22b2
Melissa Makin <melissa.makin@unimelb.edu.au>
2020-06-30 12:06:24 +1000

    added Italy as option in base.html, and don't show row.names when writing to csv

--- createCSV.R
+++ createCSV.R
@@ -10,17 +10,17 @@ dates <- unique(originalData$date)
 seq <- c(1:length(dates))
 
 dataConfirmed <- originalData %>% select(administrative_area_level_2, date, confirmed)
-outputConfirmed <- data.frame('Province.State' = states)
+outputConfirmed <- data.frame('Province/State' = states, 'Country/Region' = replicate(length(states),countryName))
 for (value in seq) {
   outputConfirmed[[format(dates[value], '%-m/%d/%y')]] <- subset(dataConfirmed, dataConfirmed$date == dates[value])$confirmed
 }
-write.csv(outputConfirmed, paste0('csvData/',countryName, '_confirmed.csv'), quote=FALSE)
+write.csv(outputConfirmed, paste0('csvData/',countryName, '_confirmed.csv'), quote=FALSE, row.names=FALSE)
 
 dataDeaths <- originalData %>% select(administrative_area_level_2, date, deaths)
-outputDeaths <- data.frame('Province.State' = states)
+outputDeaths <- data.frame('Province/State' = states, 'Country/Region' = replicate(length(states),countryName))
 for (value in seq) {
-  outputDeaths[[format(dates[value], '%-m/%d/%y')]] <- subset(dataConfirmed, dataConfirmed$date == dates[value])$confirmed
+  outputDeaths[[format(dates[value], '%-m/%d/%y')]] <- subset(dataDeaths, dataDeaths$date == dates[value])$deaths
 }
-write.csv(outputDeaths, paste0('csvData/',countryName, '_deaths.csv'), quote=FALSE)
+write.csv(outputDeaths, paste0('csvData/',countryName, '_deaths.csv'), quote=FALSE, row.names=FALSE)
 
 }
