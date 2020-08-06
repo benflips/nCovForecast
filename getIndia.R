f94532b829e9f921d6ed1d9cb9b5765b48aab609
Melissa Makin <melissa.makin@unimelb.edu.au>
2020-08-07 07:42:49 +1000

    change Indian data source to covid19datahub

--- getIndia.R
+++ getIndia.R
@@ -1,7 +1,2 @@
 source('getDataGeneral.R')
-
-getDataGeneral('India',
-               'https://raw.githubusercontent.com/vipinbhatnagar/covid19/master/confirmed.csv',
-               'https://raw.githubusercontent.com/vipinbhatnagar/covid19/master/deaths.csv',
-               '',
-               TRUE)
+getDataCovid19datahubWithoutRecovered('India')
