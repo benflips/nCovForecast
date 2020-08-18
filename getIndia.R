05013338311ffce8c25fbd1e5cde74e5257db3c5
Melissa Makin <melissa.makin@unimelb.edu.au>
2020-08-18 15:22:26 +1000

    get Indian data from vipinbhatnagar, like we have for most of the history of this project

--- getIndia.R
+++ getIndia.R
@@ -1,2 +1,7 @@
 source('getDataGeneral.R')
-getDataCovid19datahubWithoutRecovered('India')
+
+getDataGeneral('India',
+               'https://raw.githubusercontent.com/vipinbhatnagar/covid19/master/confirmed.csv',
+               'https://raw.githubusercontent.com/vipinbhatnagar/covid19/master/deaths.csv',
+               '',
+               TRUE)
